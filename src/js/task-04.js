ab777c7d3eeebc6570ee2cf4768ff4ad6689f9b7
Hostess27 <pash.pash22173@gmail.com>
2021-09-17 14:41:31 +0300

    Начало 4го таска

---
+++ src/js/task-04.js
@@ -0,0 +1,10 @@
+const counterValue = document.querySelector("#value");
+console.log(counterValue)
+const buttonDown = document.querySelector('[data-action]')
+console.log(buttonDown)
+const buttonUp = document.querySelector('[data-action]')
+console.log(buttonUp)
+
+
+
+
