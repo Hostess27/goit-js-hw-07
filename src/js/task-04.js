668ef1fff31a65f935b101ba3ce99feb69c8c84c
Hostess27 <pash.pash22173@gmail.com>
2021-09-18 16:36:30 +0300

    Таск 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,10 +1,17 @@
-const counterValue = document.querySelector("#value");
-console.log(counterValue)
-const buttonDown = document.querySelector('[data-action]')
-console.log(buttonDown)
-const buttonUp = document.querySelector('[data-action]')
-console.log(buttonUp)
+const refs = {
+    counterValue: document.querySelector("#value"),
+    buttonDown: document.querySelector('button[data-action="decrement"]'),
+    buttonUp: document.querySelector('button[data-action="increment"]'),
+};
 
+let counterValue = 0;
 
+refs.buttonDown.addEventListener("click", () => {
+    counterValue -= 1;
+    refs.counterValue.textContent = counterValue;
+});
 
-
+refs.buttonUp.addEventListener("click", () => {
+    counterValue += 1;
+    refs.counterValue.textContent = counterValue;
+});
