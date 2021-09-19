f9f527a3d7909cd93967e8f92a84c9d341111898
Hostess27 <pash.pash22173@gmail.com>
2021-09-19 14:13:10 +0300

    Правки таск 7

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,6 +1,6 @@
 const slider = document.getElementById('font-size-control');
 
 slider.addEventListener('input', () => {
-    const size = slider.value;
+    const size = event.target.value;
     text.style.fontSize = size + "px";
 });
