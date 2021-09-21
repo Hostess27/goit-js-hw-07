098eee64f2cce0c5fd04d8f143b1082a470d1acf
Hostess27 <pash.pash22173@gmail.com>
2021-09-21 21:08:50 +0300

    Правки таск 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,32 +1,46 @@
-const inputValue = () => (amountDiv = inputEl.value | 0);
+const INITIAL = 30;
+const controls = document.querySelector('[id="controls"]');
+const input = controls.querySelector("input");
+const boxPlus = controls.querySelector('button[data-action="render"]');
+const boxMinus = controls.querySelector('button[data-action="destroy"]');
+const divBoxesEl = document.querySelector('[id="boxes"]');
+
+const itemSize = {
+    value: INITIAL,
+    increment() {
+        this.value += 10;
+    },
+    reset() {
+        this.value = INITIAL;
+    },
+};
 
 const createBoxes = (amount) => {
-    const sizeDiv = 30;
-    const delta = 10;
-    const divBoxes = new Array(amount).fill(0).map((_, index) => {
-        const elem = document.createElement("div");
-        elem.style.backgroundColor = `#${Math.random()
-            .toString(16)
-            .slice(3, 9)}`;
-        elem.style.width = sizeDiv + delta * index + "px";
-        elem.style.height = elem.style.width;
-        return elem;
-    });
-    divBoxesEl.append(...divBoxes);
+    const arrayOfDiv = [];
+    for (let i = 0; i < amount; i++) {
+        const div = document.createElement("div");
+        div.style.width = `${itemSize.value}px`;
+        div.style.height = `${itemSize.value}px`;
+        div.style.backgroundColor = randomColor();
+        itemSize.increment();
+        arrayOfDiv.push(div);
+    }
+
+    divBoxesEl.append(...arrayOfDiv);
 };
 
+const callback = () => {
+    createBoxes(Math.round(input.value));
+};
 const destroyBoxes = () => {
     divBoxesEl.innerHTML = "";
+    itemSize.reset();
 };
-
-const divBoxesEl = document.querySelector("#boxes");
-const inputEl = document.querySelector("input[type = 'number']");
-
-let amountDiv = 0;
-inputEl.addEventListener("blur", inputValue.bind(inputEl));
-
-const boxPlus = document.querySelector('button[data-action="render"]');
-const boxMinus = document.querySelector('button[data-action="destroy"]');
-
-boxPlus.addEventListener("click", () => createBoxes(amountDiv));
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+};
+boxPlus.addEventListener("click", callback);
 boxMinus.addEventListener("click", destroyBoxes);
