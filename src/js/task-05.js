ba7da057b0559fc1a714b4fc8e3169e15c1bd82e
Hostess27 <pash.pash22173@gmail.com>
2021-09-21 19:11:34 +0300

    Правки таск 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,13 +1,15 @@
 const refs = {
     inputText: document.querySelector("#name-input"),
     userName: document.querySelector("#name-output"),
+};
+
+refs.inputText.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+    if (refs.inputText.value === "") {
+        refs.userName.innerHTML = `незнакомец`;
+    } else {
+        refs.userName.textContent = event.currentTarget.value;
+    }
 }
-
-
-refs.inputText.addEventListener("input", onInputChange)
-
-
-function onInputChange(event){
-console.log(event.currentTarget.value)
- refs.userName.textContent = event.currentTarget.value;
-}
+refs.userName.addEventListener("input", onInputRemove);
